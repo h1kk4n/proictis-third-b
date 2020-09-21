9ac271ab9fcc6dce129986c8f523e3d578b4fbf4
h1kk4n <62308800+h1kk4n@users.noreply.github.com>
2020-09-22 00:34:46 +0300

    silliest syntax and pep8 mistakex in contacts_handler fixed

--- app/handlers/contacs_handler.py
+++ app/handlers/contacs_handler.py
@@ -1,7 +1,8 @@
-#Не реализовал текст приветствия команды и как и в мануале подключение файлов через os
+# Не реализовал текст приветствия команды и как и в мануале подключение файлов через os
 import os.path
-from telegram.ext import InlineKeyboardButton
-from telegram.ext import InlineKeyboardMarkup
+import datetime
+from telegram import InlineKeyboardButton
+from telegram import InlineKeyboardMarkup
 from telegram.ext import CommandHandler
 from telegram.ext import CallbackQueryHandler
 
@@ -15,37 +16,39 @@ TITLES = {
   CALLBACK_BUTTON3: "Самойленко Валерия Сергеевна",
 }
 
-def get_contacs_keyboard():
-  keyboard = [
-    [InlineKeyboardButton(TITLES[CALLBACK_BUTTON1], callback_data=CALLBACK_BUTTON1)],
-    [InlineKeyboardButton(TITLES[CALLBACK_BUTTON2], callback_data=CALLBACK_BUTTON2)],
-    [InlineKeyboardButton(TITLES[CALLBACK_BUTTON2], callback_data=CALLBACK_BUTTON2)],
-    ]
-  return InlineKeyboardMarkup(keyboard)
 
-def keyboard_callback_handler(bot: Bot, update: Update, chat_data=None, **kwargs):
-  query = update.callback_query
-  data = query.data
-  now = datetime.datetime.now()
+def get_contacts_keyboard():
+    keyboard = [
+        [InlineKeyboardButton(TITLES[CALLBACK_BUTTON1], callback_data=CALLBACK_BUTTON1)],
+        [InlineKeyboardButton(TITLES[CALLBACK_BUTTON2], callback_data=CALLBACK_BUTTON2)],
+        [InlineKeyboardButton(TITLES[CALLBACK_BUTTON2], callback_data=CALLBACK_BUTTON2)],
+    ]
+    return InlineKeyboardMarkup(keyboard)
+
+
+def keyboard_callback_handler(bot, update, chat_data=None, **kwargs):
+    query = update.callback_query
+    data = query.data
+    now = datetime.datetime.now()
   
-  chat_id = update.effective_message.chat_id
-  current_text = update.effective_message.text
+    chat_id = update.effective_message.chat_id
+    current_text = update.effective_message.text
   
-  if data = CALLBACK_BUTTON1:
+    if data == CALLBACK_BUTTON1:
         query.edit_message_text(
-            text = "",
-            chat = chat_id,
-            reply_markup=get_contacs_keyboard(),
+            text="",
+            chat=chat_id,
+            reply_markup=get_contacts_keyboard(),
             )
-  elif data = CALLBACK_BUTTON2:
+    elif data == CALLBACK_BUTTON2:
         query.edit_message_text(
-            text = "",
-            chat = chat_id,
-            reply_markup=get_contacs_keyboard(),
+            text="",
+            chat=chat_id,
+            reply_markup=get_contacts_keyboard(),
             )
-  elif data = CALLBACK_BUTTON3:
+    elif data == CALLBACK_BUTTON3:
         query.edit_message_text(
-            text = "",
-            chat = chat_id,
-            reply_markup=get_contacs_keyboard(),
+            text="",
+            chat=chat_id,
+            reply_markup=get_contacts_keyboard(),
             )
